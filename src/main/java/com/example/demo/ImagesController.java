a683571a433241ca0c8b72c78dc91ad7bcb8baf3
EmmanuelMontes111 <emmauelmontes111@gmail.com>
2024-06-10 23:34:01 -0500

    initial configuration

---
+++ src/main/java/com/example/demo/ImagesController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.image.ImageClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class ImagesController {
+
+
+    private final ImageClient imageClient;
+
+    public ImagesController(ImageClient imageClient) {
+        this.imageClient = imageClient;
+    }
+
+
+    @GetMapping("/generate")
+    public String getUrlImage(@RequestParam("text") String text) {
+        return this.imageClient.call(new ImagePrompt(text)).getResult().getOutput().getUrl();
+    }
+}
